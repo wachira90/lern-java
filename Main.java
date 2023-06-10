3f1a799906c5a7a7852b268339208d173b03ec8e
Wachira Duangdee <wcrcorp@gmail.com>
2023-06-10 15:37:53 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+  public static void main(String[] args) {
+    int myNum = 15;
+    System.out.println(myNum);
+
+    int Num1;
+    Num1 = 17;
+    System.out.println(Num1);
+
+    int Num2 = 5;
+    float myFloatNum = 5.99f;
+    char myLetter = 'D';
+    boolean myBool = true;
+    String myText = "Hello";
+
+    System.out.println("Hello World");
+    System.out.println(3 + 3);
+    System.out.println(2 * 5);
+
+    String firstName = "John ";
+    String lastName = "Doe";
+    String fullName = firstName + lastName;
+    System.out.println(fullName);
+  }
+}
